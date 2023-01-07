3993d25885d3b70652beef758faf027cac028a1e
Will Paceley <pace0033@algonquinlive.com>
2023-01-07 12:50:43 -0500

    update initial view to table view and add basic trip model

---
+++ TravelJournal/Trip.swift
@@ -0,0 +1,15 @@
+//
+//  Trip.swift
+//  TravelJournal
+//
+//  Created by Will Paceley on 2023-01-06.
+//
+
+import Foundation
+
+struct Trip {
+    var departureDate: Date
+    var returnDate: Date
+    var destination: String
+    var reason: String
+}
