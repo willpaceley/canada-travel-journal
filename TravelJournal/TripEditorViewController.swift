e922034d8187610dc6af838946f935a50199eb6e
Will Paceley <pace0033@algonquinlive.com>
2023-01-07 17:18:19 -0500

    add TripEditor to storyboard with associated view controller

---
+++ TravelJournal/TripEditorViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TripEditorViewController.swift
+//  TravelJournal
+//
+//  Created by Will Paceley on 2023-01-07.
+//
+
+import UIKit
+
+class TripEditorViewController: UIViewController {
+    var isNewTrip = false
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = isNewTrip ? "Add New Trip" : "Edit Trip"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TravelJournal/ViewController.swift
+++ TravelJournal/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UITableViewController {
         
         let newTrip = Trip(departureDate: Date(timeIntervalSinceNow: 1), returnDate: Date(timeIntervalSinceNow: 2), destination: "USA", reason: "Shopping")
         trips.append(newTrip)
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(openTripEditor))
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -29,6 +31,13 @@ class ViewController: UITableViewController {
         cell.detailTextLabel?.text = trip.reason
         
         return cell
+    }
+    
+    @objc func openTripEditor() {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "TripEditor") as? TripEditorViewController {
+            vc.isNewTrip = true
+            navigationController?.pushViewController(vc, animated: true)
+        }
     }
 }
 
