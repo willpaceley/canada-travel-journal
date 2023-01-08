7d209b15f1242bd710e3d103445c5a50bceed0de
Will Paceley <pace0033@algonquinlive.com>
2023-01-08 11:02:55 -0500

    populate UIPickerView with countries data source

--- TravelJournal/TripEditorViewController.swift
+++ TravelJournal/TripEditorViewController.swift
@@ -7,24 +7,45 @@
 
 import UIKit
 
-class TripEditorViewController: UIViewController {
+class TripEditorViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     var isNewTrip = false
+    var countries = [String]()
 
+    @IBOutlet var saveButton: UIButton!
+    @IBOutlet var countryPicker: UIPickerView!
+    @IBOutlet var reasonField: UITextField!
+    @IBOutlet var returnPicker: UIDatePicker!
+    @IBOutlet var departurePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = isNewTrip ? "Add New Trip" : "Edit Trip"
+        
+        // Populate countries array
+        for code in NSLocale.isoCountryCodes  {
+            let id = NSLocale.localeIdentifier(fromComponents: [NSLocale.Key.countryCode.rawValue: code])
+            let name = NSLocale(localeIdentifier: "en_CA").displayName(forKey: NSLocale.Key.identifier, value: id) ?? "Country not found for code: \(code)"
+            countries.append(name)
+        }
+        
+        // Sort the countries alphabetically
+        countries.sort()
+        
+        // Set the view controller as delegate of the UIPickerView
+        countryPicker.delegate = self
+        countryPicker.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
-    */
-
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return countries.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return countries[row]
+    }
 }
